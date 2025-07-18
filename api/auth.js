bb3136c887c0202574252984d63cd0f2934ed19c
LykovaYuliya <iam.god.of.rhythm@gmail.com>
2025-07-19 03:24:41 +0500

    Final config for Decap CMS auth

--- api/auth.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.writeHead(302, {
-    Location: "https://decapcms-oauth.deno.dev/auth/github?client_id=Ov23lisdzdmHbcCzxbhI&redirect_uri=https://lykova-site.vercel.app/admin/"
-  });
-  res.end();
-}
--- src/pages/api/auth.js
+++ src/pages/api/auth.js
@@ -1,5 +1,4 @@
-// src/pages/api/auth.js
-export async function GET() {
+export async function GET({ request }) {
   return Response.redirect(
     "https://decapcms-oauth.deno.dev/auth/github?client_id=Ov23lisdzdmHbcCzxbhI&redirect_uri=https://lykova-site.vercel.app/admin/",
     302
