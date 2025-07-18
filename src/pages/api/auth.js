d456e022e91e39053c66b2087dba3ec7434c5868
LykovaYuliya <iam.god.of.rhythm@gmail.com>
2025-07-19 03:48:33 +0500

    Добавлен API-роут для авторизации Decap CMS

--- src/pages/api/auth.js
+++ src/pages/api/auth.js
@@ -1,5 +1,10 @@
+// src/pages/api/auth.js
 import { handleRequest } from 'decap-cms-oauth-github';
 
-export async function GET({ request }) {
+export async function GET(request) {
+  return handleRequest(request);
+}
+
+export async function POST(request) {
   return handleRequest(request);
 }
